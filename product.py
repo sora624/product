a1bcfd657090b830c7ebc052a73c077d93fe7113
sora624 <sorathai624@gmail.com>
2022-04-25 21:07:32 +0800

    01

--- product.py
+++ product.py
@@ -1,14 +1,20 @@
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '求Item,Price' in line:
+            continue
+        name, price = line.strip().split(',')
+        # products.append(name, price)
+        products.append([name, price])
+print(products)
+
 while True:
-    name = input ('Please input product name: ')
+    name = input('Please input product name: ')
     if name == 'q':
         break
     price = input('Please input price: ')
-    # item = name + price
-    item = []
-    item.append(name)
-    item.append(price)
-    products.append(item)
+    price = int(price)
+    products.append([name, price])
 
 # b = input('check item')
 print(products)
@@ -16,6 +22,7 @@ print(products)
 for p in products:
     print(p[0], " Price is", p[1])
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('求Item,Price, '\n')
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        f.write(p[0] + ',' + str(p[1]) + '\n')
